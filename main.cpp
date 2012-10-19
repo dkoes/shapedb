1c73ea50ec5a2d2ce63689ee3dd373c7613c7f3b
David Koes <dkoes@pitt.edu>
2012-10-19 12:14:56 -0400

    license

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
 /*
+ ShapeDB
+ Copyright (C) 2011  David Ryan Koes and the University of Pittsburgh
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
  * main.cpp
  *
  *  Created on: Sep 12, 2011
@@ -8,12 +27,9 @@
  a GSS-tree index for fast (and exact) similarity searching
  http://dx.doi.org/10.1145/304181.304219
 
- I have severe doubts that this will be able to scale the way I need it too,
- but am going to give it a shot.
-
- Since I intend to use this mostly as a library, the input of the primary
- routines is a list of spheres with radii already provided with a fixed
- coordinate system.  (But main will take in molecules).
+ At some point I intend to properly break this out into a library for generic
+ shape handling, but at the moment everything assumes we are performing
+ molecular shape matching.
  */
 
 #include <iostream>
